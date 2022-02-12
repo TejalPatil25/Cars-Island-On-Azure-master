9d890da3a0c76d8ebcdc2323be22611958d12adf
TejalPatil25 <tejalspatil25@gmail.com>
2022-02-12 13:18:34 +0530

    CarsIsland Commit

---
+++ Cars-Island-On-Azure-master/src/web-api/CarsIsland.API/Core/DependencyInjection/AuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CarsIsland.API.AuthorizationPolicies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+
+namespace CarsIsland.API.Core.DependencyInjection
+{
+    public static class AuthenticationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthenticationWithAuthorizationSupport(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddMicrosoftIdentityWebApiAuthentication(config, "AzureAdB2C");
+
+            services.AddSingleton<IAuthorizationHandler, ScopesHandler>();
+
+            return services;
+        }
+    }
+}
+
